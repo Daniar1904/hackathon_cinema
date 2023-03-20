1c92267857c9f1e4a36f85d645c8a690db1cd206
Nurgazy <amanbekovnurgazy@mail.ru>
2023-03-20 20:34:56 +0600

    add model movies

--- Cinema/urls.py
+++ Cinema/urls.py
@@ -35,11 +35,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-     path('admin/', admin.site.urls),
-     path('api/v1/accounts/', include('account.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
+    path('api/v1/accounts/', include('account.urls')),
+    path('api/v1/', include('movies.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
